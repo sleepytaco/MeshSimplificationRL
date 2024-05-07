8b61c435ab92e9d5ea30f89b9df97d405f363949
Mohammed Khan <abukhan363@gmail.com>
2024-05-07 08:05:07 -0400

    replace td3 with sac for AgentV2

--- AgentV2/sac.py
+++ AgentV2/sac.py
@@ -1,60 +1,59 @@
-# TD3 implementation from SB3 library. I set up a custom gym env class called MeshEnv that is suTD3rted by SB3.
+# DDPG implementation from SB3 library. I set up a custom gym env class called MeshEnv that is supported by SB3.
 # MeshEnv relies on the C++ RL environment I created called "meshenv" which is a API server that the env interfaces with
 import torch.nn as nn
-from stable_baselines3 import TD3
-from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback
+from stable_baselines3 import SAC
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 from custom_env_v2 import MeshEnv
 import time
 from stable_baselines3.common.env_checker import check_env
 
 # saved model zip to use
-model_zip = "TD3_mesh_simplify_1M_default"
+model_zip = "sac_mesh_simplify_10M_2layer"
 
 isTraining = True
 final_face_count = 200
 usedSavedModelForTraining = False
 
 # pass to model.learn
-log_name = "TD3_qem_run_default_policy"  # "TD3_approx_error_run",
-timesteps = 1_000_000
+log_name = "sac_qem_run_2layer_policy"  # "sac_approx_error_run",
+timesteps = 2_000_000
 checkpoint_callback = CheckpointCallback(
-    save_freq=100_000,  # Save a checkpoint every 1000 steps
-    save_path="./TD3_logs_default/",
-    name_prefix="TD3_rl_model",
+    save_freq=1_000_000,  # Save a checkpoint every 1000 steps
+    save_path="./sac_logs_2layer/",
+    name_prefix="sac_rl_model",
 )
 
-# pass to TD3 model or model.load
-kwargs = {
+# pass to DDPG model or model.load
+kwargs2layer = {
     "policy": "MlpPolicy",
-    # "max_steps": 512,
-    # "policy_kwargs": dict(net_arch=dict(pi=[512, 256, 128], vf=[512, 256, 128])),
-    "buffer_size": 96_000,
+    # "policy_kwargs": dict(activation_fn=nn.ReLU, net_arch=dict(pi=[128, 128],qf=[128, 128])), using default one of [400, 300] units works wondders
     "verbose": 2,
     "gamma": 0.999,  # default 0.99
-    # "batch_size": 512,  # default 256 for TD3
-    "tensorboard_log": "./TD3_logs_default/"
+    "batch_size": 128,  # --- 256 had worked well (but was slow 26 it/s) --- will try 128
+    "tensorboard_log": "./sac_logs_2layer/",
+    "buffer_size": 96_000,
 }
+kwargs = kwargs2layer
 
 if isTraining:
     print("Training...")
-    env = MeshEnv(mesh_files=[
-        "/Users/mohammedk/Documents/Brown/CS2951F/MeshSimplificationRL/AgentV1/meshes/rabbit/test/T73.obj"],
+    env = MeshEnv(mesh_files="/Users/mohammedk/Documents/Brown/CS2951F/MeshSimplificationRL/AgentV1/meshes/train",
                   final_face_count=final_face_count, training=isTraining, version=2)
 
-    # sanity check if my custom env follows the Gym interface that SB3 suTD3rts and output additional warnings if needed
+    # sanity check if my custom env follows the Gym interface that SB3 supports and output additional warnings if needed
     check_env(env)
 
     start_time = time.time()
 
     if usedSavedModelForTraining:
         print("Using saved model...", model_zip)
-        model = TD3.load(model_zip, env=env, **kwargs)
+        model = SAC.load(model_zip, env=env, **kwargs)
     else:
-        model = TD3(env=env, **kwargs)
+        model = SAC(env=env, **kwargs)
 
     model.learn(total_timesteps=timesteps, progress_bar=True, tb_log_name=log_name, callback=checkpoint_callback)
-    model.save(f"TD3_mesh_simplify_1M_default")
+    model.save(f"sac_mesh_simplify_10M_2layer")
 
     env.close()
 
@@ -74,7 +73,7 @@ else:
     env = MeshEnv(mesh_files=[mesh_file], final_face_count=final_face_count, training=isTraining, version=2)
     print("Created env with test file", mesh_file)
 
-    model = TD3.load(model_zip, env=env, **kwargs)
+    model = SAC.load(model_zip, env=env, **kwargs)
     print("Loaded saved model", model_zip)
 
     obs, info = env.reset()
